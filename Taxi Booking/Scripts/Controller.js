44c4804c0165213fceb368095171831340fa3190
Unknown <greentag741@gmail.com>
2020-03-11 13:53:38 +0000

    Working and linked files

--- Taxi Booking/Scripts/Controller.js
+++ Taxi Booking/Scripts/Controller.js
@@ -1,9 +1,34 @@
-﻿angular.module("taxiBooking").controller("taxiBookingController", function ($scope, $http)) {
+﻿angular.module("taxiBookingApp").controller("taxiBookingController", function ($scope, $http) {
+
+    
+
+    $scope.changeView = function (view) { 
+        $scope.viewBookings = false;
+        $scope.addBookings = false;
+        $scope.viewBookings = true;
+
+    };
+    $scope.init = function () {
+  
+        $scope.viewBookings = false;
+        $scope.addBookings = false;
+    }
+
+    $scope.initialise = function () {
+        $http.get("") //bookings REST API
+            .success(function (a) {
+
+            })
+            .error(function (error) {
+            $scope.errorMessage = error; // create error message view
+
+        });
+
+    }
 
 
 
 
 
 
-
-}
+});
