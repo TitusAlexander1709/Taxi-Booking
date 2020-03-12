f962cfc65ee319e01f687c5455c012cf7aacb8be
Maxwell Allen <maxwallen78@gmail.com>
2020-03-12 13:37:51 +0000

    Edit Change
    Tried adding Edit functionality. Did not work.

--- Taxi Booking/Scripts/Controller.js
+++ Taxi Booking/Scripts/Controller.js
@@ -1,21 +1,21 @@
 ﻿angular.module("taxiBookingApp").controller("taxiBookingController", function ($scope, $http) {
 
-    
+    var bookingID;
 
-    $scope.changeView = function (view) { 
+    $scope.changeView = function (view) {
         $scope.viewBookings = false;
         $scope.addBookings = false;
         $scope.viewRoutes = false;
         $scope.viewVehicles = false;
+        $scope.viewEditBookings = false;
 
-
-
-        $scope[view]  = true;
+        $scope[view] = true;
     };
 
+ 
 
     $scope.initialise = function () {
-        $http.get("http://webteach_net.hallam.shu.ac.uk/cmsds/api/booking") //bookings REST API
+        $http.get("http://webteach_net.hallam.shu.ac.uk/cmsds/api/booking/") //bookings REST API
             .success(function (response) {
                 $scope.booking = response;
             })
@@ -26,34 +26,49 @@
 
     };
 
-    $scope.initialise();
+    $scope.showBookEdit = function (id) {
+        $http.get("http://webteach_net.hallam.shu.ac.uk/cmsds/api/booking/" + id)
+            .success(function (response) {
+                bookingID = id;
+                $scope.editCurrentPassenger = response.CurrentPassenger;
+                $scope.editBookingDropOff = response.DropOffLocation;
+                $scope.editPassengerName = response.PassengerName;
+                $scope.editPickupLocation = response.PickupLocation;
+                $scope.editVehicleId = response.VehicleId;
+            })
+            .error(function (error) {
+                $scope.errorMessage = error;
+            });
+    };
 
-    $scope.addBooking = function () {
-        var bookingDetails = {
-            currentPassenger: $scope.itineraries.length,
-            dropOffLocation: $scope.itineraryName,
-            Id: $scope.itineraryDestination,
-            passengerName: $scope.itineraryPurpose,
-            pickupLocation: new Date($scope.itineraryStartDate),
-            vehicleId: new Date($scope.itineraryEndDate)
+    $scope.editBooking = function () {
+        var changedBooking = {
+            Id: bookingID,
+            CurrentPassenger: $scope.editCurrentPassenger,
+            DropOffLocation: $scope.editBookingDropOff,
+            PassengerName: $scope.PassengerName,
+            PickupLocation: $scope.PickupLocation,
+            VehicleId: $scope.VehicleId
         };
-        $scope.itineraries.push($scope.itineraryDetails)      
-    };
 
-    $scope.editBook = function (bookings) {
-
-    };
-
-    $scope.deleteBook = function (bookings) {
-        var bookingToDel = $scope.booking.indexOf(bookings);
-        $scope.booking.splice(bookingToDel, 1); //Remove 1 item @ index
-    };
+        $http.put("http://webteach_net.hallam.shu.ac.uk/cmsds/api/booking/", changedBooking)
+            .success(function (response) {
+                $scope.initialise();
+            })
+            .error(function (error) {
+                $scope.errorMessage = error;
+            });
 
 
+        $scope.deleteBook = function (bookings) {
+            var bookingToDel = $scope.booking.indexOf(bookings);
+            $scope.booking.splice(bookingToDel, 1); //Remove 1 item @ index
+        };
 
+        $scope.initialise();
 
+    }
+})
 
 
     
-
-});
